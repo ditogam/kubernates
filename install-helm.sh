f07f68fe4b392ca33a9d2ed5f108f246c60d822d
ditogam <ditogam@gmail.com>
2019-10-27 18:18:08 +0400

    Update install-helm.sh

--- install-helm.sh
+++ install-helm.sh
@@ -17,7 +17,8 @@ kubectl -n kube-system delete serviceaccount tiller
 curl -L https://git.io/get_helm.sh | bash
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller --upgrade --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | sed 's@  replicas: 1@  replicas: 1\n  selector: {"matchLabels": {"app": "helm", "name": "tiller"}}@' |kubectl apply -f -
+#helm init --service-account tiller --upgrade --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | sed 's@  replicas: 1@  replicas: 1\n  selector: {"matchLabels": {"app": "helm", "name": "tiller"}}@' |kubectl apply -f -
+helm init --service-account tiller --skip-refresh
 kubectl -n kube-system patch deploy/tiller-deploy -p '{"spec": {"template": {"spec": {"serviceAccountName": "tiller"}}}}'
 kubectl -n kube-system patch deployment tiller-deploy -p '{"spec": {"template": {"spec": {"automountServiceAccountToken": true}}}}'
 helm repo update
